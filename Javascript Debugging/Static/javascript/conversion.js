23e49f4d6dd79dbcedcef3df8a10264a2be9d04e
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2022-08-03 10:09:50 +0200

    Add files via upload

---
+++ Javascript Debugging/Static/javascript/conversion.js
@@ -0,0 +1,8 @@
+document.getElementById('convert').onclick = function () {
+	let km = document.getElementById('km').value;
+	let miles = km / 1.609;
+	miles = miles.toFixed(4);
+	console.log(km + ' kilometers');
+	console.log(miles + ' miles');
+	document.getElementById('res').innerHTML = ' ' + miles + ' miles';
+};
